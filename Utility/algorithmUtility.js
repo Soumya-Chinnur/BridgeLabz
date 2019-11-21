6c53cf627aa46000ee38d7f6ad5fece592d1b3a7
PrafulGupta <ismpraful@gmail.com>
2019-11-21 19:18:09 +0530

    [Soumya] implemented business logic

--- Utility/algorithmUtility.js
+++ Utility/algorithmUtility.js
@@ -1,80 +1,67 @@
-exports.primeGenerate=(number)=>
-{ 
-    const primeArray=[];
-    for(let i = 1; i<=number; i++)
-    {
+exports.primeGenerate = (number) => {
+    const primeArray = [];
+    for (let i = 1; i <= number; i++) {
         const temp = 0;
-        for(let j = 2; j<=i-1 ;j++)
-        {
-            if(i%j==0)
-            {
+        for (let j = 2; j <= i - 1; j++) {
+            if (i % j == 0) {
                 temp = temp + 1;
             }
         }
-            if(temp == 0)
-            {
-                //console.log(i)
-               primeArray.push(i);
-            }
+        if (temp == 0) {
+            //console.log(i)
+            primeArray.push(i);
+        }
     }
     return primeArray
 }
-exports.anagramprime = (primeArray)=>
-{
-    const anagram=[];
-for(let j = 0; j<primeArray.length; j++)
-{
-    for(let k = j + 1; k<primeArray.length; k++)
-    {
-        if(checkAnagram(primeArray[j], primeArray[k])){
-            anagram.push(primeArray[j]);
-            anagram.push(primeArray[k])
+exports.anagramprime = (primeArray) => {
+    const anagram = [];
+    for (let j = 0; j < primeArray.length; j++) {
+        for (let k = j + 1; k < primeArray.length; k++) {
+            if (checkAnagram(primeArray[j], primeArray[k])) {
+                anagram.push(primeArray[j]);
+                anagram.push(primeArray[k])
+            }
         }
     }
-}
-return anagram;
+    return anagram;
 }
 /*
 *CheckAnagram
 */
-checkAnagram = (numberOne, numberTwo)=>{
-    numberOne = ''+numberOne;
-    numberTwo = ''+numberTwo;
+checkAnagram = (numberOne, numberTwo) => {
+    numberOne = '' + numberOne;
+    numberTwo = '' + numberTwo;
     numberOneArray = numberOne.split('');
     numberTwoArray = numberTwo.split('');
     numberOneArraySort = this.bubbleSort(numberOneArray);
     numberTwoArraySort = this.bubbleSort(numberTwoArray);
-    if(numberOneArraySort.join() == numberTwoArraySort.join())
-    return true;
+    if (numberOneArraySort.join() == numberTwoArraySort.join())
+        return true;
     return false;
 }
 /*
 *bubblesort
 */
-exports.bubbleSort=(a)=>
-{
-  //  var a = [36, 19, 29, 12, 5];
-    const temp;
-    for(let i=0; i<a.length; i++)
-    {
-        for(let j=0; j<a.length-1-i; j++)
-        {
+exports.bubbleSort = (a) => {
+    //  var a = [36, 19, 29, 12, 5];
+    const temp = 0;
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < a.length - 1 - i; j++) {
             let flag = 0;
-            if(a[j]>a[j+1])
-            {
+            if (a[j] > a[j + 1]) {
                 temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
                 flag = 1;
             }
 
         }
-        if(flag==0)
-        {
+        if (flag == 0) {
             break;
         }
     }
-   return a
+    return a
 }
 /*
 Anagram
@@ -137,7 +124,7 @@ Bubblesort
 */
 exports.bubbleSort = (number) => {
     const a = [36, 19, 29, 12, 5];
-    const temp;
+    const temp = 0;
     for (let i = 0; i < number; i++) {
         for (let j = 0; j < number - 1 - i; j++) {
             let flag = 0;
@@ -163,7 +150,8 @@ Insertion sort
 exports.insertionSort = (number) => {
     try {
         const a = [5, 1, 6, 2, 4, 3];
-        const temp, j;
+        const temp = 0;
+        const j = 0;
         for (let i = 1; i < number; i++) {
             temp = a[i];
             j = i;
@@ -181,3 +169,103 @@ exports.insertionSort = (number) => {
         console.log(err);
     }
 }
+/*
+Merge Sort
+*/
+//Merges two subarrays of arr[]
+//first subarray is arr[1..m]
+//second subarray is arr[m+1..r]
+function merge(unsortedArray, low, mid, high) {
+    //Find size of two subbarray to be merged
+    var lengthOne = mid - low + 1;
+    var lengthTwo = high - mid;
+
+    /*Create temporary arrays*/
+    const leftArray = new Array(lengthOne);
+    const rightArray = new Array(lengthTwo);
+
+    /*Copy data to temporary arrays*/
+    for (let i = 0; i < lengthOne; ++i)
+        leftArray[i] = unsortedArray[low + i];
+    for (let j = 0; j < lengthTwo; ++j)
+        leftArray[i] = unsorteddArray[mid + 1 + j];
+
+    /*Merg the temporary arrays
+    
+    /*Intialize indexes of first and csecond array*/
+    var i = 0, j = 0;
+
+    /*Intialize index of merged array*/
+    var k = low;
+    while (i < lengthOne && j < lengthTwo) {
+        if (leftArray[i].localeCompare(rightarray[j]) <= 0) {
+            unsortedArray[k] = leftArray[i];
+            i++;
+        }
+        else {
+            unsortedArray[k] = rightArray[j];
+            j++;
+        }
+        k++
+    }
+
+    /* Copy remaning element of L[] if any */
+    while (i < lengthOne) {
+        unsortedArray[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    /* Copy remaining elements of R[] if any */
+    while (j < lengthTwo) {
+        unsortedArray[k] = rightArray[j];
+        j++;
+        k++;
+
+    }
+}
+
+
+//returns sorted array
+exports.mergeSort = (unsortedArray, low, high) => {
+    if (low < high) {
+        //Find middle point
+        var mid = parseInt((low + high) / 2);
+
+        //sort first and second halves
+        mergeSort(unsortedArray, low, mid);
+        mergeSort(unsortedArray, mid + 1, high);
+
+        //merge the sorted halves
+        merge(unsortedArray, low, mid, high);
+    }
+    return unsortedArray;
+}
+
+/*
+permution 
+ */
+exports.getAllPermutation = (string) => {
+    var results = [];
+
+    if (string.length === 1) {
+        results.push(string);
+        return results;
+    }
+}
+
+for (var i = 0; i < string.length; i++) {
+    //console.log("i is"+i)
+    const firstChar = string[i];
+    //console.log("first char "+firstchar)
+    var charsLeft = string.substring(0, i) + string.substring(i + 1);
+    //console.log("chars left "+charsleft)
+    var innerpermutations = getAllpermuation(charsleft);
+    //console.log("inner "+innerPermutations)
+    for (let j = 0; j < innerpermutations.length; j++) {
+        //console.log(" j"+j)
+        results.push(firstChar + innerPermutations[j]);
+        //console.log('result for'+result)
+    }
+}
+return results;
