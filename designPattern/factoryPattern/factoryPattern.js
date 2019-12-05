bf31123d6525ad23a9bbcbcda208ec0bf75e321b
PrafulGupta <soumyachinnur@gmail.com>
2019-12-05 09:36:27 +0530

    [Soumya] business logic changed for factory pattern

--- designPattern/factoryPattern/factoryPattern.js
+++ designPattern/factoryPattern/factoryPattern.js
@@ -3,7 +3,7 @@
 * @description :Prints the factory pattern design
 * @overview : Design patterns
 * @author : Soumya Chinnur
-******************************************************************************************/
+*************************************************************************************/
 try {
     const FactoryObjects = require('../factory/factoryBL')
     new FactoryObjects.PC().system();
