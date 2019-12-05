d0356f699516b7e6c4b637cb2522dabfcc0a02c4
PrafulGupta <soumyachinnur@gmail.com>
2019-12-05 18:23:04 +0530

    [Soumya] business logic changed for stock managment

---
+++ Stock/stock.js
@@ -0,0 +1,14 @@
+/****************************************************************************************
+* @Execution : default node : cmd> stock.js
+* @description :calculate the value of each stock and total value
+* @overview : To print the stock report
+* @author : Soumya Chinnur
+******************************************************************************************/
+try {
+    const stock = require('../stockManagement/stockBL')
+    let obj = new stock.stock_mgt()
+    obj.jsonParse('/home/admin1/Desktop/Programming/objectOrientedBranch/stockManagement/stock.json')
+    obj.value()
+} catch (e) {
+    console.log(e.message)
+}
