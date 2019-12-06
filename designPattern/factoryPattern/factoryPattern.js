eb128213eff13cf5578e81dccf0f9894ec0f457d
PrafulGupta <soumyachinnur@gmail.com>
2019-12-06 17:16:35 +0530

    [Soumya] Refactor.some program and pushed it into remote

--- designPattern/factoryPattern/factoryPattern.js
+++ designPattern/factoryPattern/factoryPattern.js
@@ -3,7 +3,7 @@
 * @description :Prints the factory pattern design
 * @overview : Design patterns
 * @author : Soumya Chinnur
-*************************************************************************************/
+***************************************************************/
 try {
     const FactoryObjects = require('../factoryPattern/factoryPatternBL')
     new FactoryObjects.PC().system();
