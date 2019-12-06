d261e4c6af6e7b4109b7f3fe9f07fd9b4409342d
PrafulGupta <soumyachinnur@gmail.com>
2019-12-06 18:25:45 +0530

    [Soumya] Remote.some programs and pushed into remote

--- unOrderList/unOrderedList.js
+++ unOrderList/unOrderedList.js
@@ -1,3 +1,12 @@
+/*************************************************************
+ * @purpose:Read the Text from a file, split it into words and 
+ *          arrange it as Linked List. Take a user input to search
+ *          a Word in the List. If the Word is not found then add
+ *          it to the list, and if it found then remove the word from the List.
+ *           In the end save the list into a file
+ * @author:Soumya Chinnur
+ * @file:unOrderedList.js
+ * *******************************************************/
 const fs = require("fs");//built in library to read file
 let linkedList = require('../unOrderList/unOrderedListBL');
 let readline = require("readline-sync");
