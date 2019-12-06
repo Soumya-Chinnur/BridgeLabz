0f3e125266630fc5312521fb0c5d753b80cf2778
PrafulGupta <soumyachinnur@gmail.com>
2019-12-06 13:57:41 +0530

    [Soumya] business logic changed for prototype pattern

--- designPattern/prototypePattern/prototypePattern.js
+++ designPattern/prototypePattern/prototypePattern.js
@@ -2,7 +2,7 @@
 *@purpose :Demonstrate prototype pattern.(clone the existing object into a new object)
 * file :prototypePattern.js
 * author :Soumya Chinnur
-*****************************************************************************************/
+*************************************************************************************/
 function student(name, age, add) {
     this.name = name;
     this.age = age;
@@ -14,13 +14,13 @@ function studentPrototype(proto) {
     this.proto = proto;
     this.clone = function () {
         let std = new student();
-        std.name = proto.name;
+        std.name = proto.name; //copying values from proto type object
         std.age = proto.age;
         return std;
     };
 }
 function run() {
-    let proto = new student('Soumya', '24');
+    let proto = new student('Soumya', '24'); //creating student object
     let prototype = new studentPrototype(proto);
     let std = prototype.clone();
     std.say();
