706f32b633b89f1ad298b79b75b438816bb31da5
PrafulGupta <soumyachinnur@gmail.com>
2019-12-06 18:06:38 +0530

    [Soumya] Remote.some programs and pushed into remote

--- primeAnagramQueue/primeAnaQueue.js
+++ primeAnagramQueue/primeAnaQueue.js
@@ -1,10 +1,10 @@
-/*******************************************************************************************
+/*****************************************************************
  * @purpose : Add the Prime Numbers that are Anagram in the Range of 0 - 1000 in a Queue 
  *            using the Linked List and Print the Anagrams from the Queue. Note no
  *            Collection Library can be used.
  * @Author : Soumya Chinnur
  * @file : primeAnaQueue.js
- **********************************************************************************/
+ ****************************************************************/
 const primeAnagram = require('../../Functional/PrimeAnagram');
 const primeAnaQueue = require('../primeAnagramQueue/primeAnaQueueBL');
 try {
