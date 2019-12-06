90082551bad65bc0183ff9b6d650bc8dcaa98427
PrafulGupta <soumyachinnur@gmail.com>
2019-12-06 18:01:39 +0530

    [Soumya] Remote.some programs and pushed into remote

--- Balancedparanthesis/balancedParanthesis.js
+++ Balancedparanthesis/balancedParanthesis.js
@@ -1,5 +1,7 @@
 /**********************************************************
- * @purpose:
+ * @purpose:Take an Arithmetic Expression such as 
+ *          (5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3) where parentheses are used 
+ *          to order the performance of operations. Ensure parentheses must appear in a balanced fashion.
  * @author: Soumya Chinnur
  * @file: balancedParanthesis.js
  ************************************************************/
