fbe07eb0e3a4bafe4489c5a3f023e536f35a51e6
PrafulGupta <soumyachinnur@gmail.com>
2019-12-06 18:02:32 +0530

    [Soumya] Remote.some programs and pushed into remote

--- orderedList/orderedList.js
+++ orderedList/orderedList.js
@@ -1,3 +1,12 @@
+/************************************************************
+ * @purpose: Read .a List of Numbers from a file and arrange it 
+ *           ascending Order in a Linked List. Take user input
+ *           for a number, if found then pop the number out of 
+ *           the list else insert the number in appropriate 
+ *           position
+ * @author:Soumya Chinnur
+ * @file: orderedListBl.js
+ ************************************************************/
 const fs = require("fs"); //built in library for read file
 const linkedList = require('./orderedListBL'); //import the orderedlinkedlist.js file
 const readline = require("readline-sync");
